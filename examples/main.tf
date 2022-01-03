747630db76767dbfe33b7688d0fb30982f2d1954
Tamas Erdei <tamas.erdei@pan-net.eu>
2022-01-03 15:11:51 +0100

    Implement user, project member and external network RBAC resources

--- examples/main.tf
+++ examples/main.tf
@@ -1,13 +1,25 @@
 terraform {
   required_providers {
     juice = {
-      version = "~> 0.1"
+      version = "~> 0.2"
       source = "terraform.in.pan-net.eu/juice/juice"
+    }
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0.0"
     }
   }
 }
 
 provider "juice" {
+}
+
+provider "random" {
+}
+
+resource "random_password" "password" {
+  length   = 20
+  special  = false
 }
 
 # Create sample project
@@ -16,6 +28,25 @@ resource "juice_project" "project" {
   # cloud = "juice-cloud" 
   name  = "tf-juice-test"
   description = "Test project for TF juice provider"
+}
+
+# Create project user
+resource "juice_user" "project_user" {
+  # "cloud" attribute is automatically set from J_CLOUD env var
+  # cloud = "juice-cloud" 
+  username    = "tf-juice-test-user1"
+  password    = random_password.password.result
+  description = "Test user for TF juice provider"
+}
+
+# Add a member user
+resource "juice_project_member" "project_member" {
+  juice_project_id = juice_project.project.id
+  username = juice_user.project_user.username
+  # user_domain = "users_domain"
+  depends_on = [
+    juice_user.project_user
+  ]
 }
 
 # Set project quotas
@@ -50,4 +81,10 @@ resource "juice_quota" "quota" {
   floatingips          = 1
   security_groups      = -1
   security_group_rules = -1
+}
+
+# Create RBAC for external network
+resource "juice_extnet_rbac" "pi-net" {
+  juice_project_id = juice_project.project.id
+  network = "external_pi-net_provider"
 }

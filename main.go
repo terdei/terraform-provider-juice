bf49f218cb9a63ef38d9b0c519fb38d4268e6d1c
Tamas Erdei <tamas.erdei@pan-net.eu>
2024-07-23 00:51:44 +0200

    Update module and module references to MCICD
    Also update CI config to match the MCICD env.

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	"gitlab.tools.in.pan-net.eu/juice/terraform-provider-juice.git/juice"
+	"gitlab.devops.telekom.de/juice/terraform-provider-juice.git/juice"
 )
 
 func main() {
